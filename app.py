022c5e83a3d741e0f9bedb8f2b3ad52b8ef672de
Lucas Andriotti Ciamaricone <lucasandriotticiamaricone@gmail.com>
2026-03-05 21:42:55 -0300

    Atualização Aula 03

--- app.py
+++ app.py
@@ -29,5 +29,30 @@ def exibe_cor(cor1):
 def exibe_cor2(cor1, cor2):
     return f'<h1 style="color:{cor1}">A cor foi: {cor1} e <b style="color:{cor2}">{cor2}</b></h1>'
 
+@app.route('/codigos/<int:ano_nascimento>')
+def exibe_codigos(ano_nascimento):
+    dados = {
+        'nome' : "Lucas",
+        'ano_nascimento' : ano_nascimento,
+        'idade' : (2026 - ano_nascimento),
+        'texto' : "Vamos escrever um texto bem longo para que o truncate funcione!",
+        'ativo' :'erro',
+        'pessoas' : ['Lucas', 'Rafel', 'Douglas']
+    }
+
+    return render_template('codigos.html', **dados)
+
+@app.route('/home')
+def home():
+    return render_template('blocos/home.html')
+
+@app.route('/produtos')
+def produtos():
+    return render_template('blocos/produtos.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('blocos/perfil.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
